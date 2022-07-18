6c8bcf446318b0cc9f907460f0f115e8c564b8a0
Владислав Антонов <lolik1806@gmail.com>
2022-07-18 11:38:50 +0300

    create matrix

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,47 @@
 <template>
-    <div class="border-2 m-2 border-beige rounded-lg h-auto w-2/3">
+    <div class="border-2 m-2 border-beige rounded-lg h-auto w-1/2">
         <div class="p-2">
             <label>Количество вершин: </label>
             <input class="w-9" type="number" min="2" v-model="vertexCount" />
         </div>
         <div class="p-2">
-            <div>{{ matrix }}</div>
+            <div v-for="(_, i) in matrix">
+                <div class="inline-block" v-for="(_, j) in matrix[i]">
+                    <input @input="changeArrayValue" class="w-9" type="number" min="0" v-model="matrix[i][j]" />
+                </div>
+            </div>
         </div>
     </div>
-    <div class="border-2 m-2 border-beige rounded-lg h-auto w-1/3">
-
-    </div>
+    <div class="border-2 m-2 border-beige rounded-lg h-auto w-1/2"></div>
 </template>
 
+/**
+
+ */
+
 <script>
+import { SendMatrix } from './api/BruteForceApi';
+
 export default {
     data() {
         return {
             vertexCount: 2,
+            matrix: []
         };
     },
-    computed: {
-        matrix() {
-            let m = new Array(this.vertexCount);
-
-            for (let i = 0; i < m.length; i++) {
-                m[i] = new Array(this.vertexCount).fill(0);
-            }
-
-            return m;
+    methods: {
+        changeArrayValue(event) {
+            console.log(event.target.value);
         }
+    },
+    watch: {
+        vertexCount(newValue) {
+            this.matrix = new Array(newValue);
+
+            for (let i = 0; i < this.matrix.length; i++) {
+                this.matrix[i] = new Array(newValue).fill(0);
+            }
+        },
     }
 };
 </script>
---
+++ src/api/BruteForceApi.js
@@ -0,0 +1,6 @@
+function SendMatrix(matrix) {
+
+}
+
+
+export { SendMatrix }
